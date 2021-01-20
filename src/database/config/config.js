2aa76b2e379bf812e6d3b7fc0535d0df6b5a4f70
Bramirez <bramirezsm14@gmail.com>
2021-01-20 17:32:47 -0300

    config edited

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,19 +5,11 @@ module.exports={
     "database": "clqb1mn8vn9rhwb2",
     "host": "lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
+    
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    production: {
+    use_env_variable: 'JAWSDB_URL',
+    dialect: 'mysql'
   }
+  
 }
